3204a0708b80fb1776ad98ecd33183a31a6f5f4f
singhyashe15 <singhyashraj574@gmail.com>
2025-07-08 00:23:56 +0530

    socket

--- codeEditor_backend/socket/socket.js
+++ codeEditor_backend/socket/socket.js
@@ -3,6 +3,7 @@ import { Server } from "socket.io";
 import http from "http";
 import roomModel from "../models/room.js";
 import dotenv from "dotenv";
+import { connect } from "http2";
 
 dotenv.config();
 
@@ -18,10 +19,16 @@ const io = new Server(server, {
 });
 
 const roomChats = {};
+const adminSocketId = new Map();
 
 io.on("connection", (socket) => {
-  socket.on("code-room", (id) => {
-    socket.join(id);
+  socket.on("code-room", async ({ roomId, userId }) => {
+    socket.join(roomId);
+    const room = await roomModel.findById({ _id: roomId });
+    
+    if (room.adminId.toString() === "686256c0c774740ef46e70d5") {
+      adminSocketId.set(roomId, socket.id)
+    }
   })
 
   socket.on('real-time-code-sent', async ({ code, roomId }) => {
@@ -33,13 +40,27 @@ io.on("connection", (socket) => {
     }
   })
 
-  socket.on('chat', ({roomId , name ,id, text}) => {
+  socket.on('chat', ({ roomId, name, id, text }) => {
     if (!roomChats[roomId]) roomChats[roomId] = [];
 
-    roomChats[roomId].push({id ,name , text});
+    roomChats[roomId].push({ id, name, text });
     io.to(roomId).emit('received-chat', roomChats[roomId]);
   });
 
+  socket.on('request-join-room', ({ roomId, name, id }) => {
+    
+    const adminId = adminSocketId.get(roomId);
+    const requestId = socket.id;
+    io.to(adminId).emit('join-request', { name, id, requestId });
+  })
+
+  socket.on("respond-join-request", ({ requestId, isApproved }) => {
+    if (isApproved === true) {
+      io.to(requestId).emit("join-approved");
+    } else {
+      io.to(requestId).emit("join-denied", { reason: "Rejected by admin" });
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log(`Socket ${socket.id} disconnected`);
